fb81bbfb41f79ccd0dd86f7366369e506c0471bc
yhk0014 <yhk0014@miraesoft.kr>
2011-11-13 10:24:09 +0900

    김어준 추가

--- HeadFirstJava/src/main/main.java
+++ HeadFirstJava/src/main/main.java
@@ -6,7 +6,7 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("³ª´Â ²Ä¼ö´Ù"); 
+		System.out.println("³ª´Â ²Ä¼ö´Ù ±è¾îÁØ ±è¾îÁØ"); 
 
 	}
 
